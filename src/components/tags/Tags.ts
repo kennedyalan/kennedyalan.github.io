bc372ad588653746e007318b66e238fabeb73f49
Alan Kennedy <Alaneduardokennedy@gmail.com>
2025-10-12 02:00:03 -0300

    new version

---
+++ src/components/tags/Tags.ts
@@ -0,0 +1,81 @@
+import CSS from "../icons/CSS.astro";
+import Docker from "../icons/Docker.astro";
+import HTML from "../icons/HTML.astro";
+import Java from "../icons/Java.astro";
+import Javascript from "../icons/Javascript.astro";
+import MySQL from "../icons/MySQL.astro";
+import Next from "../icons/Next.astro";
+import PostgreSQL from "../icons/PostgreSQL.astro";
+import Rabbitmq from "../icons/Rabbitmq.astro";
+import React from "../icons/React.astro";
+import Spring from "../icons/Spring.astro";
+import TypeScript from "../icons/TypeScript.astro";
+import TailwindCSS from "../icons/TailwindCSS.astro";
+
+export const TAGS = {
+    JAVA: {
+      name: "Java",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Java,
+    },
+    SPRING: {
+      name: "Spring",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Spring,
+    },
+    MYSQL: {
+      name: "MySQL",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: MySQL,
+    },
+    POSTGRESQL: {
+      name: "PostgreSQL",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: PostgreSQL,
+    },
+    RABBITMQ: {
+      name: "Rabbitmq",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Rabbitmq,
+    },
+    DOCKER: {
+      name: "Docker",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Docker,
+    },
+    NEXT: {
+      name: "Next.js",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Next,
+    },
+    REACT: {
+      name: "React",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: React,
+    },
+    HTML: {
+      name: "HTML",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: HTML,
+    },
+    CSS: {
+      name: "CSS",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: CSS,
+    },
+    TAILWIND: {
+      name: "TailwindCSS",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: TailwindCSS,
+    },
+    JAVASCRIPT: {
+      name: "JavaScript",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: Javascript,
+    },
+    TYPESCRIPT: {
+      name: "TypeScript",
+      class: "bg-black text-white dark:bg-gray-800",
+      icon: TypeScript,
+    },
+};
