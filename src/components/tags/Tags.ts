e92ef5552129b859ba370a33a71cd4dd3aa4e182
Alan Kennedy <alaneduardokennedy@gmail.com>
2026-03-17 18:28:50 -0300

    change projects

--- src/components/tags/Tags.ts
+++ src/components/tags/Tags.ts
@@ -15,67 +15,67 @@ import TailwindCSS from "../icons/TailwindCSS.astro";
 export const TAGS = {
     JAVA: {
       name: "Java",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-red-100 text-red-800 dark:bg-red-950 dark:text-red-300",
       icon: Java,
     },
     SPRING: {
       name: "Spring",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-green-100 text-green-800 dark:bg-green-950 dark:text-green-300",
       icon: Spring,
     },
     MYSQL: {
       name: "MySQL",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-sky-100 text-sky-800 dark:bg-sky-950 dark:text-sky-300",
       icon: MySQL,
     },
     POSTGRESQL: {
       name: "PostgreSQL",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-indigo-100 text-indigo-800 dark:bg-indigo-950 dark:text-indigo-300",
       icon: PostgreSQL,
     },
     RABBITMQ: {
-      name: "Rabbitmq",
-      class: "bg-black text-white dark:bg-gray-800",
+      name: "RabbitMQ",
+      class: "bg-orange-100 text-orange-800 dark:bg-orange-950 dark:text-orange-300",
       icon: Rabbitmq,
     },
     DOCKER: {
       name: "Docker",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-blue-100 text-blue-800 dark:bg-blue-950 dark:text-blue-300",
       icon: Docker,
     },
     NEXT: {
       name: "Next.js",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200",
       icon: Next,
     },
     REACT: {
       name: "React",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-cyan-100 text-cyan-800 dark:bg-cyan-950 dark:text-cyan-300",
       icon: React,
     },
     HTML: {
       name: "HTML",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-orange-100 text-orange-800 dark:bg-orange-950 dark:text-orange-300",
       icon: HTML,
     },
     CSS: {
       name: "CSS",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-sky-100 text-sky-800 dark:bg-sky-950 dark:text-sky-300",
       icon: CSS,
     },
     TAILWIND: {
       name: "TailwindCSS",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-teal-100 text-teal-800 dark:bg-teal-950 dark:text-teal-300",
       icon: TailwindCSS,
     },
     JAVASCRIPT: {
       name: "JavaScript",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-yellow-100 text-yellow-800 dark:bg-yellow-950 dark:text-yellow-300",
       icon: Javascript,
     },
     TYPESCRIPT: {
       name: "TypeScript",
-      class: "bg-black text-white dark:bg-gray-800",
+      class: "bg-blue-100 text-blue-800 dark:bg-blue-950 dark:text-blue-300",
       icon: TypeScript,
     },
 };
